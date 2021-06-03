efcd8b403741f48dd484798620d318d48bb41347
Marcelo Ribeiro <marcelogrsp@gmail.com>
2021-06-03 20:44:23 +0000

    CORS issue fixed

--- controller.py
+++ controller.py
@@ -1,27 +1,39 @@
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from models import Indicator
 from config import conn_string
 from datetime import timedelta
 from app import app, db
+
 import wsgiserver
 import services
 import hashlib
 import os
 
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route("/api/authenticate", methods=["POST"])
+@cross_origin()
 def login():
+    print(request.remote_addr)
     username = request.json["username"]
     password_hash = hashlib.pbkdf2_hmac('sha256', request.json["password"].encode('utf-8'), b'bytemine', 100000).hex()
     password_hash_verify = hashlib.pbkdf2_hmac('sha256', "bytemine".encode('utf-8'), b'bytemine', 100000).hex()
 
     if password_hash == password_hash_verify:
         access_token = create_access_token(identity=username, expires_delta=timedelta(days=1))
-        return jsonify(message="Authenticated", access_token=access_token), 200
+        
+        res = jsonify(message="Authenticated", access_token=access_token)
+        res.status_code = 200
     else:
-        return jsonify(message="Incorrect Credentials"), 403
+        res = jsonify(message="Incorrect Credentials")
+        res.status_code = 403
+    return res
 
 @app.route('/api/indicators', methods=['GET', 'POST'])
+@cross_origin()
 @jwt_required()
 def indicators():
     if request.method == 'GET':
@@ -38,6 +50,7 @@ def indicators():
         return jsonify(indicator_model), 201
 
 @app.route('/api/indicators/metadata/<unique_key>/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def indicators_metadata_unique_key(unique_key):
     output = services.get_indicators_metadata(unique_key)
@@ -45,6 +58,7 @@ def indicators_metadata_unique_key(unique_key):
     return jsonify(output), 200
 
 @app.route('/api/indicators/metadata/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def indicators_metadata():
     output = services.get_indicators_metadata()
@@ -52,6 +66,7 @@ def indicators_metadata():
     return jsonify(output), 200
 
 @app.route('/api/indicators/validate/', methods=['POST'])
+@cross_origin()
 @jwt_required()
 def validate_indicators():
     validation_test = services.validate_indicator(request.get_json())
@@ -62,6 +77,7 @@ def validate_indicators():
         return jsonify(error=validation_test['data']), 400
 
 @app.route('/api/indicators/<id>/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def metadata():
     print(request.view_args['id'])
@@ -80,6 +96,7 @@ def metadata():
     return res
 
 @app.route('/api/logics/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def logics():
     output = services.get_logics()
@@ -87,6 +104,7 @@ def logics():
     return jsonify(output), 200
 
 @app.route('/api/logics/<id>/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def logics_id(id):
     output = services.get_logics(id)
@@ -94,6 +112,7 @@ def logics_id(id):
     return jsonify(output), 200
 
 @app.route('/api/actions/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def actions():
     output = services.get_actions()
@@ -101,6 +120,7 @@ def actions():
     return jsonify(output), 200
 
 @app.route('/api/actions/<id>/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def actions_id(id):
     output = services.get_actions(id)
@@ -108,6 +128,7 @@ def actions_id(id):
     return jsonify(output), 200
 
 @app.route('/api/datas/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def datas():
     output = services.get_datas()
@@ -115,6 +136,7 @@ def datas():
     return jsonify(output), 200
 
 @app.route('/api/datas/<id>/', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def datas_id(id):
     output = services.get_datas(id)
@@ -122,6 +144,7 @@ def datas_id(id):
     return jsonify(output), 200
 
 @app.route('/api/search/<keyword>', methods=['GET'])
+@cross_origin()
 @jwt_required()
 def search(keyword):
     output = services.search(keyword)
@@ -144,5 +167,5 @@ if __name__ == "__main__":
         app.run()
     else:
         print("Starting WSGI...")
-        server = wsgiserver.WSGIServer(app, host='0.0.0.0', port=5000)
+        server = wsgiserver.WSGIServer(app, host="0.0.0.0", port=5000)
         server.start()
